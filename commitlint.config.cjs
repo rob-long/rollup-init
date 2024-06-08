3d35c3a64639b68091d5905fe5913ca6d5531d6b
Rob Long <roblong@gmail.com>
2024-06-08 16:20:39 -0700

    feat: Setup commit-msg precommit

---
+++ commitlint.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore']],
+    'subject-case': [2, 'always', ['sentence-case']],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72]
+  }
+};
